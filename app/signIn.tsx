3f07c313450caca3c07d8521ca3e8dc237cb2330
Saad Kashif <saad220905@gmail.com>
2024-08-05 09:14:08 -0400

    implement the welcome screen and add the app icons

---
+++ app/signIn.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ScreenWrapper from '@/components/ScreenWrapper'
+
+const SignIn = () => {
+  return (
+    <ScreenWrapper>
+      <Text>SignIn</Text>
+    </ScreenWrapper>
+  )
+}
+
+export default SignIn
+
+const styles = StyleSheet.create({})
