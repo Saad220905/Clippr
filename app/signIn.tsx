57cc7aed0f3410e0260c15e46467d6ec7794736a
Saad Kashif <saad220905@gmail.com>
2024-11-29 21:41:09 -0500

    modify containers

--- app/signIn.tsx
+++ app/signIn.tsx
@@ -4,6 +4,8 @@ import ScreenWrapper from '@/components/ScreenWrapper'
 import { StatusBar } from 'expo-status-bar'
 import BackButton from '@/components/BackButton'
 import { useRouter } from 'expo-router'
+import { height_percentage, width_percentage } from '@/helpers/common'
+import { theme } from '@/constants/theme'
 
 const SignIn = () => {
   const router = useRouter();
@@ -12,6 +14,11 @@ const SignIn = () => {
       <StatusBar style='dark' />
       <View style={styles.container}>
         <BackButton router={router}/>
+        {/* welcome */}
+        <View>
+          <Text style={styles.welcomeText}>Hey,</Text>
+          <Text style={styles.welcomeText}>Welcome Back</Text>
+        </View>
       </View>
     </ScreenWrapper>
   )
@@ -21,6 +28,32 @@ export default SignIn
 
 const styles = StyleSheet.create({
   container: {
-    
+    flex: 1,
+    gap: 45,
+    paddingHorizontal: width_percentage(5)
+  },
+  welcomeText: {
+    fontSize: height_percentage(4),
+    fontWeight: theme.fonts.bold,
+    color: theme.colors.text
+  },
+  form: {
+    gap: 25
+  },
+  forgotPassword: {
+    textAlign: 'right',
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5
+  },
+  footerText: {
+    textAlign: 'center',
+    color: theme.colors.text,
+    fontSize: height_percentage(1.6)
   }
 })
